72cd51c169a151960e757e3e376628921c5bb231
Omar-farid <86683278+omarfarids@users.noreply.github.com>
2022-09-03 02:12:12 +0200

    creating component and using react diagrams

---
+++ app-ui/src/components/BasicConnection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Shape from './Shape'
+
+
+function BasicConnection() {
+  return (
+    <div>
+        <Shape />
+    </div>
+  )
+}
+
+export default BasicConnection
